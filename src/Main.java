78ca466750e87aa2e1601304c52b625f39d7fa65
Athari Muhammad <70275655+InnovaReborn2GD@users.noreply.github.com>
2024-10-25 22:07:25 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,6 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Contoh array
-
         int[] evenNumbers = getEvenNumbers(arr); // Memanggil fungsi
 
         // Menampilkan bilangan genap
